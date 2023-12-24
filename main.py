f54d643f12cc98ef204f29cd5c03ac0256908b73
DollM <duicn187532@gmail.com>
2023-12-25 01:40:00 +0800

    圖

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import json
